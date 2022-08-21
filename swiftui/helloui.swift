ae7b55efef07696246577e29a2ad512a7fafb8c7
pablo <pablo53@poczta.onet.eu>
2022-08-22 00:38:46 +0200

    SwiftUI

--- swiftui/helloui.swift
+++ swiftui/helloui.swift
@@ -1,35 +1,51 @@
-import Cocoa
+import SwiftUI
 
+// Inspired by https://gist.github.com/chriseidhof/26768f0b63fa3cdf8b46821e099df5ff
 
+struct HelloView: View {
+    var body: some View {
+        VStack {
+            Text("Hello World!")
+                .padding()
+                .background(Capsule().fill(Color.red))
+                .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)        
+    }
+}
 
-let quitMenuItem = NSMenuItem.init(
-    title: "Quit hello",
-    action: #selector(NSApplication.terminate),
-    keyEquivalent: "q"
-)
+class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
+    var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        window = NSWindow(
+            contentRect: NSRect(x: 10, y: 100, width: 320, height: 240),
+            styleMask: [.titled, .closable, .fullSizeContentView],
+            backing: .buffered,
+            defer: false
+        )
+        window.title = "Hello App"
+        window.contentView = NSHostingView(rootView: HelloView())
+        window.makeKeyAndOrderFront(nil)
+        window.delegate = self
+        NSApp.activate(ignoringOtherApps: true)
+    }
+}
 
-let appMenu = NSMenu()
-appMenu.addItem(quitMenuItem)
+let myapp = NSApplication.shared  // NSApp is the same as myapp, after this assignment
 
-let appMenuItem = NSMenuItem()
-appMenuItem.submenu = appMenu
+var customMenu: NSMenu {
+    let appMenu = NSMenuItem()
+    appMenu.submenu = NSMenu()
+    appMenu.submenu?.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+    
+    let mainMenu = NSMenu(title: "HelloWorld menu")
+    mainMenu.addItem(appMenu)
+    return mainMenu
+}
 
-let menubar = NSMenu()
-menubar.addItem(appMenuItem)
-
-let nsapp = NSApplication.shared
-nsapp.setActivationPolicy(NSApplication.ActivationPolicy.regular)
-nsapp.mainMenu = menubar
-
-let window = NSWindow.init(
-    contentRect: NSMakeRect(100, 100, 320, 200),
-    styleMask: [.titled, .fullSizeContentView],
-    backing: .buffered,
-    defer: false
-)
-window.cascadeTopLeft(from: NSMakePoint(20, 20))
-window.title = "Hello World UI Application"
-window.makeKeyAndOrderFront(nil)
-
-nsapp.activate(ignoringOtherApps: true)
-nsapp.run()
+let appDelegate = AppDelegate()
+myapp.setActivationPolicy(.regular)
+myapp.mainMenu = customMenu
+myapp.delegate = appDelegate
+myapp.run()
